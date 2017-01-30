38d4936f4ea265a2ca68cbfad147e2238a73a67c
theodot <tony.theodoropoulos@ed.amdsb.ca>
2017-01-30 13:22:02 -0500

    Cleanup of various solutions

--- DrawingStarMethod/Form1.cs
+++ DrawingStarMethod/Form1.cs
@@ -11,6 +11,7 @@ using System.Drawing;
 using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace DrawingStarMethod
@@ -28,17 +29,22 @@ namespace DrawingStarMethod
         private void drawButton_Click(object sender, EventArgs e)
         {
             Graphics formGraphics = this.CreateGraphics();
-            Pen drawPen = new Pen(Color.Black);
+            Pen drawPen = new Pen(Color.Red);
 
             float xValue, yValue, size;
 
-            xValue = Convert.ToSingle(xInput.Text);
-            yValue = Convert.ToSingle(yInput.Text);
-            size = Convert.ToSingle(sizeInput.Text);
+            //xValue = Convert.ToSingle(xInput.Text);
+            //yValue = Convert.ToSingle(yInput.Text);
+            //size = Convert.ToSingle(sizeInput.Text);
 
             formGraphics.Clear(Color.DarkKhaki);
-            DrawStar(drawPen, xValue, yValue, size);
-        }
+            //DrawStar(drawPen, xValue, yValue, size);
+            DrawStar(drawPen, 50, 50, 100);
+
+            string temp;
+
+            temp = testMeth("hello", "How are you");
+       }
 
         /// <summary>
         /// Draws an unfilled star on the screen
@@ -50,25 +56,32 @@ namespace DrawingStarMethod
         public void DrawStar(Pen starPen, float x, float y, float pixels)
         {
             Graphics formGraphics = this.CreateGraphics();
-            Pen drawPen = new Pen(Color.Black);
-            SolidBrush drawBrush = new SolidBrush(Color.Red);
+           // Pen drawPen = new Pen(Color.Black);
+          //  SolidBrush drawBrush = new SolidBrush(Color.Red);
 
             // base star is 207 pixels wide.  A scale is created based on this value to determine how long
             // to draw each side so that it will end up being the number of pixels wide that the user desires.
             float scale = pixels / 207;
 
-            formGraphics.DrawLine(drawPen, 80 * scale + x, 77 * scale + y, 103 * scale + x, 4 * scale + y);
-            formGraphics.DrawLine(drawPen, 103 * scale + x, 4 * scale + y, 126 * scale + x, 78 * scale + y);
-            formGraphics.DrawLine(drawPen, 126 * scale + x, 78 * scale + y, 207 * scale + x, 78 * scale + y);
-            formGraphics.DrawLine(drawPen, 207 * scale + x, 78 * scale + y, 143 * scale + x, 125 * scale + y);
-            formGraphics.DrawLine(drawPen, 143 * scale + x, 125 * scale + y, 167 * scale + x, 197 * scale + y);
-            formGraphics.DrawLine(drawPen, 167 * scale + x, 197 * scale + y, 103 * scale + x, 152 * scale + y);
-            formGraphics.DrawLine(drawPen, 103 * scale + x, 152 * scale + y, 40 * scale + x, 196 * scale + y);
-            formGraphics.DrawLine(drawPen, 40 * scale + x, 196 * scale + y, 63 * scale + x, 123 * scale + y);
-            formGraphics.DrawLine(drawPen, 63 * scale + x, 123 * scale + y, 0 * scale + x, 77 * scale + y);
-            formGraphics.DrawLine(drawPen, 0 * scale + x, 77 * scale + y, 80 * scale + x, 77 * scale + y);
+            Thread.Sleep(2000);
+            formGraphics.DrawLine(starPen, 80 * scale + x, 77 * scale + y, 103 * scale + x, 4 * scale + y);
+            formGraphics.DrawLine(starPen, 103 * scale + x, 4 * scale + y, 126 * scale + x, 78 * scale + y);
+            formGraphics.DrawLine(starPen, 126 * scale + x, 78 * scale + y, 207 * scale + x, 78 * scale + y);
+            formGraphics.DrawLine(starPen, 207 * scale + x, 78 * scale + y, 143 * scale + x, 125 * scale + y);
+            formGraphics.DrawLine(starPen, 143 * scale + x, 125 * scale + y, 167 * scale + x, 197 * scale + y);
+            formGraphics.DrawLine(starPen, 167 * scale + x, 197 * scale + y, 103 * scale + x, 152 * scale + y);
+            formGraphics.DrawLine(starPen, 103 * scale + x, 152 * scale + y, 40 * scale + x, 196 * scale + y);
+            formGraphics.DrawLine(starPen, 40 * scale + x, 196 * scale + y, 63 * scale + x, 123 * scale + y);
+            formGraphics.DrawLine(starPen, 63 * scale + x, 123 * scale + y, 0 * scale + x, 77 * scale + y);
+            formGraphics.DrawLine(starPen, 0 * scale + x, 77 * scale + y, 80 * scale + x, 77 * scale + y);
 
             // TODO put the above points into a PointF array and use DrawPolygon to draw your star
+        }
+
+        public string testMeth(string message1, string message2)
+        {
+            MessageBox.Show(message1 + " " + message2);
+            return "done";
         }
 
         /// Gathers information from the input boxes and then sends values
@@ -77,7 +90,7 @@ namespace DrawingStarMethod
         private void fillButton_Click(object sender, EventArgs e)
         {
             Graphics formGraphics = this.CreateGraphics();
-            Pen drawPen = new Pen(Color.Black);
+            Pen drawPen = new Pen(Color.Red);
             SolidBrush drawBrush = new SolidBrush(Color.Black);
 
             float xValue, yValue, size;
